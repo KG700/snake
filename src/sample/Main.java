f10be7044d08aa80c763d76dd78c35920369dd07
Karla Gardiner <karla.gardiner@gmail.com>
2020-10-01 15:36:01 +0100

    Set up initial variables

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,10 +6,20 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main extends Application {
 
     //variables
-    
+    static int speed = 5;
+    static int foodcolour = 0;
+    static int width = 20;
+    static int height = 20;
+    static int foodX = 0;
+    static int foodY = 0;
+    static int cornerSize = 25;
+    static List<Corner> snake = new ArrayList<>();
 
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
